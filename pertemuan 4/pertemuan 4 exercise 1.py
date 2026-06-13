39009ea15e99d3fb6c700d1381a94f891a022397
mario123504 <mariooke82@gmail.com>
2026-06-13 14:46:28 +0700

    Add files via upload

---
+++ pertemuan 4/pertemuan 4 exercise 1.py
@@ -0,0 +1,5 @@
+## For in Range
+# Syntax Type 2:
+for i in range (7,0, -1):
+    print(str(i) * i)
+
---
+++ pertemuan 4/pertemuan 4 exercise 2.py
@@ -0,0 +1,7 @@
+## For in Range
+# Syntax Type 2:
+for i in range (7,0, -1):
+    print(str(i) * i )
+for a in range (2,8, +1):
+    print(str(a) * a)
+
